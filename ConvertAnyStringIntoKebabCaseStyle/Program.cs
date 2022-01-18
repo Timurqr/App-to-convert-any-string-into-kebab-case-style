417a035bbfde7cc7ffaf0e22ad622f018a0dce80
Timurqr <berezhnoi.work@gmail.com>
2022-01-18 20:46:05 +0200

    Add project files.

---
+++ ConvertAnyStringIntoKebabCaseStyle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConvertAnyStringIntoKebabCaseStyle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
