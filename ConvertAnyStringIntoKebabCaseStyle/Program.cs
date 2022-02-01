0f07e46ff7b62f7b5f2dcd09e11356b75eb9c123
Timurqr <berezhnoi.work@gmail.com>
2022-02-01 03:06:38 +0200

    Added plain text to kebab case converter

--- ConvertAnyStringIntoKebabCaseStyle/Program.cs
+++ ConvertAnyStringIntoKebabCaseStyle/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace ConvertAnyStringIntoKebabCaseStyle
 {
@@ -6,7 +8,13 @@ namespace ConvertAnyStringIntoKebabCaseStyle
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter text to convert to kebab-case-style");
+            Console.Write("Enter text convert to kebab-case-style: ");
+            var inputText = (Console.ReadLine());
+            string pattern = @"\W";
+            string target = "-";
+            Regex regex = new Regex(pattern);
+            string result = regex.Replace(inputText, target);            
+            Console.WriteLine(result.ToLower());
         }
     }
 }
