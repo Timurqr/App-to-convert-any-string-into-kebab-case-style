855e89fffd501fbcfad9a6270b266c2694b0e559
Timurqr <berezhnoi.work@gmail.com>
2022-02-16 03:40:10 +0200

    Added exclusion of "-" from the beginning of the result and from the end

--- ConvertAnyStringIntoKebabCaseStyle/Program.cs
+++ ConvertAnyStringIntoKebabCaseStyle/Program.cs
@@ -8,12 +8,12 @@ namespace ConvertAnyStringIntoKebabCaseStyle
         static void Main(string[] args)
         {
             Console.Write("Enter text convert to kebab-case-style: ");
-            var inputText = (Console.ReadLine());
+            var inputText = (Console.ReadLine());          
             string pattern = @"[\W_]+";
             string replacement = "-";
             Regex regex = new Regex(pattern);
-            string result = regex.Replace(inputText, replacement);            
-            Console.WriteLine(result.ToLower());
+            string result = regex.Replace(inputText, replacement);           
+            Console.WriteLine(result.ToLower().Trim('-'));          
         }
     }
 }
